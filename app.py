8a0e6fc19333074b3edcee8732aea0e0c63ec395
Christian Velani <chris13082000@gmail.com>
2022-06-08 12:46:31 +0000

    Implementei o update e atualizei algumas variáveis

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 app = Flask(__name__)
 
-pedidos = [{'id' : uuid4(),'Nome da Empresa' : 'Shift Tecnologia', 'Email' : 'shift@email.com', 'Telefone' : '17954673514', 'Quantidade de Peças' : '10', 'Tipo de Peças' : 'Gola Redonda', 'Status do Pedido' : 'Entregue'}]
+pedidos = [{'id' : uuid4(),'Nome da Empresa' : 'Shift Tecnologia', 'Email' : 'shift@email.com', 'Telefone' : '17954673514', 'Quantidade de Pecas' : '10', 'Tipo de Pecas' : 'Gola Redonda', 'Status do Pedido' : 'Entregue'}]
 
 @app.route('/')
 def home():
@@ -21,7 +21,7 @@ def salvar():
     quantidade = request.form['quantidade']
     tipo = request.form['tipo']
     status = request.form['status']
-    pedidos.append({'id' : uuid4(),'Nome da Empresa' : nome, 'Email' : email, 'Telefone' : telefone, 'Quantidade de Peças' : quantidade, 'Tipo de Peças' : tipo, 'Status do Pedido' : status})
+    pedidos.append({'id' : uuid4(),'Nome da Empresa' : nome, 'Email' : email, 'Telefone' : telefone, 'Quantidade de Pecas' : quantidade, 'Tipo de Pecas' : tipo, 'Status do Pedido' : status})
     return redirect('/')
 
 @app.route('/delete/<id>')
@@ -32,6 +32,28 @@ def apagar(id):
             del pedidos[numero_pedido]
             return redirect('/')
         numero_pedido += 1
+
+@app.route('/update/<id>')
+def atualizar(id):
+    for pedido in pedidos:
+        if id == str(pedido['id']):
+            return render_template('atualizar.html', pedido=pedido)
+
+@app.route('/update/atualizar/<id>', methods=['POST'])
+def salvar_atualização(id):
+    for pedido in pedidos:
+        if id == str(pedido['id']):
+            atualizar = pedidos.index(pedido)
+            id_do_pedido = pedido['id']
+    nome = request.form['nome']
+    email = request.form['email']
+    telefone = request.form['telefone']
+    quantidade = request.form['quantidade']
+    tipo = request.form['tipo']
+    status = request.form['status']
+    pedidos[atualizar] = {'id' : id_do_pedido, 'Nome da Empresa' : nome, 'Email' : email, 'Telefone' : telefone, 'Quantidade de Pecas' : quantidade, 'Tipo de Pecas' : tipo, 'Status do Pedido' : status}
+    return redirect('/')
+
     
 
 
